f2f2da92ce401e2b26aa434c07bef702c03415f5
rmai2995 <rmai2995@sydney.uni.edu.au>
2019-12-25 23:26:58 +1100

    NavTabs

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,23 +3,38 @@ import "./App.css";
 import VideoGrid from "./VideoGrid.js";
 import Chart from "./Chart.js";
 import config from "react-global-configuration";
+import NavTabs from "./NavTabs.js";
 
 const HomePage = () => {
   const [data, setData] = useState([]);
   const [test, setTest] = useState(false);
 
+  const tabs = ["All", "International", "Super Rugby", "Premiership"];
+
   useEffect(() => {
     console.log("Use Effect run");
-    getData();
+    getData(0);
   }, []);
 
-  const getData = async () => {
-    if (test) {
-      return;
+  const getData = async index => {
+    console.log("Triggered");
+
+    var request = "";
+
+    console.log(index);
+
+    if (index == 0) {
+      request = config.get("backend_url") + "highlights/";
+    } else if (index == 1) {
+      request = config.get("backend_url") + "highlights/?league=international";
+    } else if (index == 2) {
+      request = config.get("backend_url") + "highlights/?league=superrugby";
+    } else if (index == 3) {
+      request = config.get("backend_url") + "highlights/?league=aviva";
     }
-    setTest(true);
-    console.log("Set to true");
-    const request = config.get("backend_url") + "highlights/";
+
+    console.log(request);
+
     const response = await fetch(request, {
       mode: "cors"
     });
@@ -31,13 +46,12 @@ const HomePage = () => {
 
   return (
     <div className="HomePage">
-      <VideoGrid
-        title="Recent Matches"
-        key="2"
-        data={data.matches}
-        type="match"
-      />
-      <VideoGrid title="Recent Tries" key="1" data={data.tries} type="try" />
+      <h3 className="grid-title">Recent Matches</h3>
+      <NavTabs titles={tabs} activeTab={0} changeTab={getData} />
+      <VideoGrid key="2" data={data.matches} type="match" />
+      <h3 className="grid-title">Recent Tries</h3>
+      <NavTabs titles={tabs} activeTab={0} changeTab={getData} />
+      <VideoGrid key="1" data={data.tries} type="try" />
     </div>
   );
 };
---
+++ src/NavTabs.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function NavTabs(props) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const changeTab = index => {
+    setActiveTab(index);
+    props.changeTab(index);
+  };
+
+  return (
+    <div className="NavTabs">
+      <nav>
+        <ul>
+          {props.titles &&
+            props.titles.map((title, index) => (
+              <a
+                onClick={() => changeTab(index)}
+                id={activeTab === index && "activeTab"}
+              >
+                {title}
+              </a>
+            ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavTabs;
